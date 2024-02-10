d6ba7b880a4a94c21da1ea9023a6a0aa5cd15ee5
salcover <salvakanals@gmail.com>
2024-02-10 12:08:25 +0100

    Add the new components

--- almibar-de-coco/src/components/recipes/RecipesComponent.vue
+++ almibar-de-coco/src/components/recipes/RecipesComponent.vue
@@ -1,56 +1,171 @@
 <script setup>
 import { ref } from 'vue'
 import Galleria from 'primevue/galleria'
+import DataView from 'primevue/Dataview'
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import { useI18n } from "vue-i18n";
+import '@/assets/recipes/recipes.css'
 
-const dImagenes = ref([
+const { t } = useI18n()
+
+const dDataRecipes = ref ([
     {
-        srcImage: '/imgRecetas/recetaSetas1.jpg'
+        id: 0,
+        imagenes: [
+            {
+                srcImage: '/imgRecetas/recetaSetas1.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas2.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas3.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas4.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas5.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas6.jpg'
+            }
+        ],
+        title: "Salsa de setas a la boloñesa",
+        description: "Lorem lore lore"
     },
     {
-        srcImage: '/imgRecetas/recetaSetas2.jpg'
-    },
-    {
-        srcImage: '/imgRecetas/recetaSetas3.jpg'
-    },
-    {
-        srcImage: '/imgRecetas/recetaSetas4.jpg'
-    },
-    {
-        srcImage: '/imgRecetas/recetaSetas5.jpg'
-    },
-    {
-        srcImage: '/imgRecetas/recetaSetas6.jpg'
+        id: 1,
+        imagenes: [
+            {
+                srcImage: '/imgRecetas/recetaSetas1.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas2.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas3.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas4.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas5.jpg'
+            },
+            {
+                srcImage: '/imgRecetas/recetaSetas6.jpg'
+            }
+        ],
+        title: "Salsa de setas a la boloñesa",
+        description: "Lorem lore lore"
     }
 ])
 
 </script>
 <template>
-    <div class="card grid nested-grid">
-        <div class="col-4">
-            <Galleria :value="dImagenes"  :numVisible="5" :circular="true" containerStyle="max-width: 640px max-height:450px">
-                <template #item="slotProps">
-                    <img :src="slotProps.item.srcImage" class="border-round shadow-4" style="width:400px; height: 500px; object-fit: cover; object-position: center; display:block" />
-                </template>
-                <template #thumbnail="slotProps">
-                    <div class="grid grid-nogutter justify-content-center">
-                        <img :src="slotProps.item.srcImage" style="width: 75px; height:75px; display:block"/>
+    <DataView :value="dDataRecipes" paginator :rows="5" :pt="{ content: { class: ['paginationRecipes'] } }" lazy >
+        <template #list="slotProps">
+            <div v-for="(item, index) in slotProps.items" :key="index">
+                <div class="card grid nested-grid recipeLayout">
+                    <template v-if="item.id %2 !==0">
+                    <div class="col-4">
+                        <Galleria :value="item.imagenes" :pt="{ content: { class: ['imgRecetas'] } }"  :numVisible="5" :circular="true" containerStyle="max-width: 640px max-height:450px">
+                            <template #item="slotProps">
+                                <img :src="slotProps.item.srcImage" class="shadow-7 imgPrincipal" />
+                            </template>
+                            <template #thumbnail="slotProps">
+                                <div class="grid grid-nogutter justify-content-center">
+                                    <img :src="slotProps.item.srcImage" class="imgThumbnail"/>
+                                </div>
+                            </template>
+                        </Galleria>
                     </div>
-                </template>
-            </Galleria>
-        </div>
-        <div class="col-8">
-            <div class="grid">
-                <div class="col-12" style="text-align:center">
-                    <h1>Setas a la boloñesa</h1>
+                    <div class="col-8">
+                        <div class="grid">
+                            <div class="col-12" style="text-align:center">
+                                <h1> {{ item.title }}</h1>
+                            </div>
+                            <div class="col-6">
+                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                            </div>
+                            <div class="col-6">
+                                <Accordion :activeIndex="[0]" :multiple="true" expandIcon="pi pi-plus" collapseIcon="pi pi-minus" class="tabsRecipes" >
+                                    <AccordionTab :header="t('recipes.tabs.recipe')">
+                                        <p class="m-0">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                                            laborum.
+                                        </p>
+                                    </AccordionTab>
+                                    <AccordionTab :header="t('recipes.tabs.ingredients')">
+                                        <p class="m-0">
+                                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                                            enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                                        </p>
+                                    </AccordionTab>
+                                    <AccordionTab :header="t('recipes.tabs.make')">
+                                        <p class="m-0">
+                                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
+                                            culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                                        </p>
+                                    </AccordionTab>
+                                </Accordion>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    </template>
+                    <template v-else>
+                        <div class="col-8">
+                            <div class="grid">
+                                <div class="col-12" style="text-align:center">
+                                    <h1> {{ item.title }}</h1>
+                                </div>
+                                <div class="col-6">
+                                    <Accordion :activeIndex="[0]" expandIcon="pi pi-plus" :multiple="true" collapseIcon="pi pi-minus" class="tabsRecipes" >
+                                        <AccordionTab :header="t('recipes.tabs.recipe')">
+                                            <p class="m-0">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                                                laborum.
+                                            </p>
+                                        </AccordionTab>
+                                        <AccordionTab :header="t('recipes.tabs.ingredients')">
+                                            <p class="m-0">
+                                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                                                enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                                            </p>
+                                        </AccordionTab>
+                                        <AccordionTab :header="t('recipes.tabs.make')">
+                                            <p class="m-0">
+                                                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
+                                                culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                                            </p>
+                                        </AccordionTab>
+                                    </Accordion>
+                                </div>
+                                <div class="col-6">
+                                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <Galleria :value="item.imagenes" :pt="{ content: { class: ['imgRecetas'] } }"  :numVisible="5" :circular="true" containerStyle="max-width: 640px max-height:450px">
+                                <template #item="slotProps">
+                                    <img :src="slotProps.item.srcImage" class="shadow-7 imgPrincipal" />
+                                </template>
+                                <template #thumbnail="slotProps">
+                                    <div class="grid grid-nogutter justify-content-center">
+                                        <img :src="slotProps.item.srcImage" class="imgThumbnail"/>
+                                    </div>
+                                </template>
+                            </Galleria>
+                        </div>
+                    </template>
                 </div>
-                <div class="col-6">
-                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-                </div>
-                <div class="col-6">
-                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-                </div>
-                
             </div>
-        </div>
-    </div>
+        </template>
+    </DataView>
 </template>
